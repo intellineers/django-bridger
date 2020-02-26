4cb0a5045072c3dc209924e1dbd1e8ff047b9830
Christopher <c.wittlinger@intellineers.com>
2020-02-26 12:31:17 +0100

    notification infomration

--- bridger/notifications/consumers.py
+++ bridger/notifications/consumers.py
@@ -1,4 +1,5 @@
 from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 
 from bridger.websockets.consumers import AsyncAuthenticatedJsonWebsocketConsumer
@@ -11,30 +12,33 @@ class NotificationConsumer(AsyncAuthenticatedJsonWebsocketConsumer):
         await super().connect()
         if "user" in self.scope:
             channel_layer_name = f"notification-{self.scope['user'].id}"
-            print(channel_layer_name)
             await self.channel_layer.group_add(channel_layer_name, self.channel_name)
 
-            # notification_message = await self.get_notification_message(
-            #     self.scope["user"]
-            # )
-            # await self.send_json(
-            #     {"title": "Notification Status", "message": notification_message}
-            # )
+            await self.channel_layer.group_send(
+                channel_layer_name,
+                {"type": "notification.info", "user_id": self.scope["user"].id},
+            )
 
-    # async def get_notification_message(self, user):
-    #     num_unreceived, num_unread = await database_sync_to_async(
-    #         self.get_notification_information
-    #     )(user)
-    #     return f"You have {num_unreceived} new notifications and {num_unread} unread notifications."
+    async def get_notification_message(self, user):
+        num_unreceived, num_unread = await database_sync_to_async(
+            self.get_notification_information
+        )(user)
+        return f"You have {num_unreceived} new notifications and {num_unread} unread notifications."
 
-    # def get_notification_information(self, user):
-    #     qs = Notification.objects.filter(recipient=user)
-    #     num_unreceived = qs.filter(timestamp_received__isnull=True).update(
-    #         timestamp_received=timezone.now()
-    #     )
-    #     num_unread = qs.filter(timestamp_read__isnull=True).count()
+    def get_notification_information(self, user):
+        qs = Notification.objects.filter(recipient=user)
+        num_unreceived = qs.filter(timestamp_received__isnull=True).update(
+            timestamp_received=timezone.now()
+        )
+        num_unread = qs.filter(timestamp_read__isnull=True).count()
 
-    #     return num_unreceived, num_unread
+        return num_unreceived, num_unread
+
+    async def notification_info(self, content):
+        notification_message = await self.get_notification_message(
+            get_user_model().objects.get(id=content["user_id"])
+        )
+        await self.send_json({"title": "Notification", "message": notification_message})
 
     async def notification_notify(self, content):
         await self.send_json(

13bcc161d4256b4461e3810f004bef600851d807
Christopher <c.wittlinger@intellineers.com>
2020-03-25 10:49:30 +0100

    updated frontend version and null many form data

--- bridger/serializers/fields/related.py
+++ bridger/serializers/fields/related.py
@@ -26,6 +26,10 @@ class BridgerManyRelatedField(ManyRelatedField):
         if data not in [None, empty] and len(data) == 1 and data[0] == "":
             data = []
 
+        # If the data is a list and contains the string null, then we need to convert it (FORM DATA)
+        if data == ['null']:
+            data = []
+
         # If the data is None and null is an allowed value, data needs to be set to an empty list
         if data is None and self.allow_null:
             data = []

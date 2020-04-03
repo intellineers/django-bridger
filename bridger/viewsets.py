9d14564956ba268d2b08e162869b5dba3596b2d2
Christopher <c.wittlinger@intellineers.com>
2020-04-03 17:18:59 +0200

    instance in update

--- bridger/viewsets.py
+++ bridger/viewsets.py
@@ -48,6 +48,20 @@ class InstanceMixin:
 
         return Response(serialized_content)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response({"instance": serializer.data})
+
 
 class RepresentationModelViewSet(
     MetadataMixin, InstanceMixin, viewsets.ReadOnlyModelViewSet

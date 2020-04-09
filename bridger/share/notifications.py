38fc6c13f5ee94fcc95674638c6412964c3387d9
Christopher <c.wittlinger@intellineers.com>
2020-04-09 08:21:01 +0200

    notifications show icon now

--- bridger/share/notifications.py
+++ bridger/share/notifications.py
@@ -8,5 +8,7 @@ def share_notification(user_id, widget_endpoint, message, user):
         recipient_id=user_id,
         title=f"{user.first_name} {user.last_name} shared a widget with you",
         message=message,
-        buttons=[dict(WidgetButton(label="Open", endpoint=endpoint))],
+        buttons=[
+            dict(WidgetButton(label="Open", icon="wb-icon-data", endpoint=endpoint))
+        ],
     )

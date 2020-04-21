c22783e2a840e8946a1ba71a214a2c33972a2925
Christopher <c.wittlinger@intellineers.com>
2020-04-21 14:50:24 +0200

    uniquify instance buttons

--- bridger/metadata/fields/custom_instance_buttons.py
+++ bridger/metadata/fields/custom_instance_buttons.py
@@ -1,9 +1,20 @@
-from typing import List
+from typing import List, Iterable, Dict
 
 from rest_framework.request import Request
 
 from bridger.buttons.bases import ButtonConfig as Button
 from bridger.metadata.mixins import BridgerMetadataMixin
+
+
+def unique(l: List, key: str) -> Iterable[Dict]:
+    keys = list()
+    for i in l:
+        key = i.get(key, None)
+        if key and key not in keys:
+            keys.append(key)
+            yield i
+        elif not key:
+            yield i
 
 
 class CustomInstanceButtonMetadata(BridgerMetadataMixin):
@@ -27,7 +38,7 @@ class CustomInstanceButtonMetadataMixin:
             for button in buttons:
                 button.request = request
                 button_list.append(dict(button))
-            return button_list
+            return unique(button_list, "key")
         else:
             buttons = self.get_custom_list_instance_buttons(
                 request=request,
@@ -37,4 +48,4 @@ class CustomInstanceButtonMetadataMixin:
             for button in buttons:
                 button.request = request
                 button_list.append(dict(button))
-            return button_list
+            return unique(button_list, "key")

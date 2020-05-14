e5e364944ec948c7b1a1ea32153ae41d67349be9
Christopher <c.wittlinger@intellineers.com>
2020-05-14 15:55:20 +0200

    new frontend version

--- tests/settings/base.py
+++ tests/settings/base.py
@@ -191,7 +191,7 @@ LOGGING = {
 BRIDGER_SETTINGS = {
     "FRONTEND_CONTEXT": {
         "CSS_URL": "https://atonra-stainly-cdn.fra1.cdn.digitaloceanspaces.com/static/css/main-0-5-5.css",
-        "JS_URL": "https://atonra-stainly-cdn.fra1.cdn.digitaloceanspaces.com/static/js/main-1-0-15-rc-3.js",
+        "JS_URL": "https://atonra-stainly-cdn.fra1.cdn.digitaloceanspaces.com/static/js/main-1-1-1-rc-2.js",
     }
 }
 

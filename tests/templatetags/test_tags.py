6e2c8d5ae22622c0b670ff7cc17c0763544949d6
Christopher <c.wittlinger@intellineers.com>
2020-05-15 10:22:32 +0200

    another testing templatetag

--- tests/templatetags/test_tags.py
+++ tests/templatetags/test_tags.py
@@ -15,7 +15,21 @@ class ModelTestImageNode(template.Node):
         return ""
 
 
+class ModelTestTextNode(template.Node):
+    def __init__(self, model_test):
+        self.instance = model_test
+
+    def render(self, context):
+        return self.instance.text_field or ""
+
+
 @register.tag
 def model_test_image(parser, token):
     args = token.split_contents()
     return ModelTestImageNode(ModelTest.objects.get(id=args[1]))
+
+
+@register.tag
+def model_test_text(parser, token):
+    args = token.split_contents()
+    return ModelTestTextNode(ModelTest.objects.get(id=args[1]))

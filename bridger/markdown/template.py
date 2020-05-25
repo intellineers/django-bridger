c796848ba62636e130cda44e122db3ad7a3471d7
Christopher <c.wittlinger@intellineers.com>
2020-05-25 11:14:21 +0200

    version bump and rendering in bridger

--- bridger/markdown/template.py
+++ bridger/markdown/template.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 from django.template import Context, Template
 from django.utils.safestring import SafeString
+from markdown import markdown
 
 from bridger.settings import bridger_settings
 
@@ -19,7 +20,25 @@ def compile_template_for_templatetag(templatetag: str) -> Template:
     return Template(load_registered_templatetags() + templatetag)
 
 
-def render_template_for_templatetag(templatetag: str, **context: Dict) -> SafeString:
+def render_template_for_templatetag(templatetag: str, **context) -> SafeString:
     """Renders a template for a single templatetag"""
     template = compile_template_for_templatetag(templatetag)
     return template.render(Context(context))
+
+
+def resolve_templatetags(content: str, **context) -> str:
+    """Resolves all templatetags of some content"""
+    return Template(load_registered_templatetags() + content).render(Context(context))
+
+
+def resolve_markdown(content: str, extensions=None) -> str:
+    """Resolves the markdown of some content"""
+    return markdown(content, extensions=extensions)
+
+
+def resolve_content(content: str, extensions=None, **context) -> str:
+    """Resolves the content by first resolving the templatetags and then resolving any potential markdown fields"""
+    return resolve_markdown(
+        resolve_templatetags(content, **context), extensions=extensions
+    )
+
